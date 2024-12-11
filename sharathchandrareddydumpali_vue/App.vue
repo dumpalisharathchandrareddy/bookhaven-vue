78a91712dff89b700c9e11dbbc1ddb1187664d87
Dumpali Sharath chandra Reddy <78716032+dumpalisharathchandrareddy@users.noreply.github.com>
2024-12-11 03:37:39 -0500

    vue zip

---
+++ sharathchandrareddydumpali_vue/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <MyHeader appName="BOOK HAVEN"/> 
+  <BooksList :books=books />
+  <MyFooter />
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue';
+import BooksList from './components/BooksList.vue';
+import MyFooter from './components/MyFooter.vue';
+
+
+export default{
+  name: 'App',
+  components:{
+    MyHeader,
+    BooksList,
+    MyFooter
+  },
+  data(){
+    return {
+      books: []
+    }
+  },
+  methods:{
+    async fetchBooks(){
+      const res = await fetch('https://bookshavennode.onrender.com/api');
+      const data = await res.json()
+      console.log(data)
+      return data.books;
+    }
+  },
+  async created(){
+      this.books = await this.fetchBooks();
+  }
+}
+</script>
+
+
+<style>
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+#app{
+  background-color: #c9d3ee;
+}
+</style>
---
+++ sharathchandrareddydumpali_vue/components/MyFooter.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer class="footer">  <!-- footer class -->
+      <div class="footer-content">
+        <p class="contact-info">
+          <strong>Contact Us:</strong> 
+          <a href="mailto:sharath@bookhaven.com">sharath@bookhaven.com</a>
+
+        </p>
+        <p>&copy; 2024 Book Haven. All Rights Reserved.</p>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: "MyFooter",
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    background-color: #324c62;
+    color: white;
+    padding: 20px;
+    text-align: center;
+    position: relative;
+    bottom: 0;
+    width: 100%;
+    font-size: 14px;
+    height: 5rem;
+  }
+  
+  .footer-content {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  
+  .contact-info {
+    margin: 5px 0;
+  }
+  
+  .contact-info a {
+    color: #ffffff;
+    text-decoration: none;
+  }
+  
+  .contact-info a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
